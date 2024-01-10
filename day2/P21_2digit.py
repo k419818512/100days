252d98a60ee8035c425c86d67c9d7dfef10836fb
Tao Wu <k419818512@outlook.com>
2024-01-09 19:22:37 -0500

    Create P21_2digit.py

---
+++ day2/P21_2digit.py
@@ -0,0 +1,9 @@
+two_digit_number = input("Please input two digit number:")
+two_digit_str = str(two_digit_number)
+one= int(two_digit_str[0])
+two = int(two_digit_str[1])
+twosum = one + two
+
+print("the sum of thest two digits is " + str(twosum))
+
+
