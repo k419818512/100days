2a3244373e2ecb82f8ba5b6eabbef95fbe619000
Tao Wu <k419818512@outlook.com>
2024-01-09 21:17:15 -0500

    check in

---
+++ day4/P42_random.py
@@ -0,0 +1,9 @@
+import random
+import my_module
+
+random_integer = random.random()
+random_float = random.random() * 5
+print(random_float)
+
+love_score = random.randint(1, 100)
+print (f"Your love score is {love_score}")
